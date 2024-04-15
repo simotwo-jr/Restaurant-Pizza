a9181887a6a10081846aafe8c37a5d3dee73f2ee
Nehemiah <nehemiahcheruiyot7069@gmail.com>
2024-04-15 09:58:20 +0300

    feat: Class restaurant

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizza_restaurants.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    pizzas = relationship("Pizza", secondary="restaurant_pizza")
+
