294c6ad8726316fd6b5ae22e217ba25e2a89689f
Nehemiah <nehemiahcheruiyot7069@gmail.com>
2024-04-15 10:06:15 +0300

    feat: Class restaurantpizza

--- main.py
+++ main.py
@@ -18,3 +18,11 @@ class Pizza(db.Model):
     name = db.Column(db.String(50), nullable=False)
     ingredients = db.Column(db.String(200), nullable=False)
 
+class RestaurantPizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
+    restaurant = relationship("Restaurant")
+    pizza = relationship("Pizza")
+
