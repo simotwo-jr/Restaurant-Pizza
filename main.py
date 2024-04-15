e6a5e114603b52383ffb310383904898b20ad56a
Nehemiah <nehemiahcheruiyot7069@gmail.com>
2024-04-15 10:03:46 +0300

    feat: Class pizza

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ class Restaurant(db.Model):
     address = db.Column(db.String(200), nullable=False)
     pizzas = relationship("Pizza", secondary="restaurant_pizza")
 
+class Pizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    ingredients = db.Column(db.String(200), nullable=False)
+
